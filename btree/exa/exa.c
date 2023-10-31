af3b34c2b7d5941dc3ee7f4229e1633a266986f0
Jakub Antonín Štigler <xstigl00@stud.fit.vut.cz>
2023-10-31 17:35:39 +0100

    Finish exa

--- btree/exa/exa.c
+++ btree/exa/exa.c
@@ -2,49 +2,80 @@
  * Použití binárních vyhledávacích stromů.
  *
  * S využitím Vámi implementovaného binárního vyhledávacího stromu (soubory ../iter/btree.c a ../rec/btree.c)
- * implementujte funkci letter_count. Výstupní strom může být značně degradovaný (až na úroveň lineárního seznamu) 
+ * implementujte funkci letter_count. Výstupní strom může být značně degradovaný (až na úroveň lineárního seznamu)
  * a tedy implementujte i druhou funkci (bst_balance), která strom, na požadavek uživatele, vybalancuje.
  * Funkce jsou na sobě nezávislé a tedy automaticky NEVOLEJTE bst_balance v letter_count.
- * 
+ *
  */
 
 #include "../btree.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-
 /**
  * Vypočítání frekvence výskytů znaků ve vstupním řetězci.
- * 
- * Funkce inicilializuje strom a následně zjistí počet výskytů znaků a-z (case insensitive), znaku 
- * mezery ' ', a ostatních znaků (ve stromu reprezentováno znakem podtržítka '_'). Výstup je v 
+ *
+ * Funkce inicilializuje strom a následně zjistí počet výskytů znaků a-z (case insensitive), znaku
+ * mezery ' ', a ostatních znaků (ve stromu reprezentováno znakem podtržítka '_'). Výstup je v
  * uložen ve stromu.
- * 
+ *
  * Například pro vstupní řetězec: "abBccc_ 123 *" bude strom po běhu funkce obsahovat:
- * 
+ *
  * key | value
  * 'a'     1
  * 'b'     2
  * 'c'     3
  * ' '     2
  * '_'     5
- * 
+ *
  * Pro implementaci si můžete v tomto souboru nadefinovat vlastní pomocné funkce.
 */
 void letter_count(bst_node_t **tree, char *input) {
+    for (; *input; ++input) {
+        int cnt = 0;
+        bst_search(*tree, *input, &cnt);
+        bst_insert(tree, *input, cnt + 1);
+    }
 }
 
+// Inserts `nodes` into `tree` in such order, that if the tree is empty, it
+// will be balanced
+void _bst_balance(bst_node_t **tree, bst_node_t **nodes, size_t len) {
+    if (len == 0) {
+        return;
+    }
 
+    // insert the middle node
+    size_t p = len / 2;
+    bst_insert(tree, nodes[p]->key, nodes[p]->value);
+
+    // inserts the nodes for the left and right subtrees
+    _bst_balance(tree, nodes, p);
+    _bst_balance(tree, nodes + p + 1, len - p - 1);
+}
 
 /**
  * Vyvážení stromu.
- * 
+ *
  * Vyvážený binární vyhledávací strom je takový binární strom, kde hloubka podstromů libovolného uzlu se od sebe liší maximálně o jedna.
- * 
+ *
  * Předpokládejte, že strom je alespoň inicializován. K získání uzlů stromu využijte vhodnou verzi vámi naimplmentovaného průchodu stromem.
  * Následně můžete například vytvořit nový strom, kde pořadím vkládaných prvků zajistíte vyváženost.
- *  
+ *
  * Pro implementaci si můžete v tomto souboru nadefinovat vlastní pomocné funkce. Není nutné, aby funkce fungovala *in situ* (in-place).
 */
 void bst_balance(bst_node_t **tree) {
+    // init
+    bst_items_t items = { 0 };
+    bst_node_t *res;
+    bst_init(&res);
+
+    // walk trough the tree and than insert the items in the correct order
+    bst_inorder(*tree, &items);
+    _bst_balance(&res, items.nodes, items.size);
+
+    bst_dispose(tree);
+    free(items.nodes);
+
+    *tree = res;
 }
